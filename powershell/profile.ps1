0987d0d220892639626230e71120b37b77ab8d5b
André Correia <andrecorreiait@gmail.com>
2019-12-19 15:47:23 +0000

    Create profile.ps1

---
+++ powershell/profile.ps1
@@ -0,0 +1,10 @@
+# PowerShell Core
+# Windows system
+
+# Alias
+## Github Folder
+Function FUNCGH {Set-Location -Path C:\Code\Github\}
+Set-Alias -Name gh -Value FUNCGH
+
+# Check output
+Write-Output "andxpto loaded"
