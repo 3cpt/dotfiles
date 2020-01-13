24b5ff5f214b6f8f49eaf3f89b008125c6cf3113
André Correia <andrecorreiait@gmail.com>
2020-01-13 16:29:20 +0000

    Update profile.ps1

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -10,5 +10,8 @@ Set-Alias -Name gh -Value FUNCGH
 Function FUNCGL {Set-Location -Path C:\Gitlab\}
 Set-Alias -Name gl -Value FUNCGL
 
+## Docker
+Set-Alias -Name d -Value docker
+
 # Check output
 Write-Output "andxpto configs loaded"
