e31773125295f3ae7dbba4ea3aa8d8c2ed6e150e
André Correia <andxpto@pm.me>
2020-07-07 13:00:17 +0100

    disable smart quotes

--- macosdefaults.sh
+++ macosdefaults.sh
@@ -2,3 +2,6 @@
 
 # show hidden files
 defaults write com.apple.Finder AppleShowAllFiles true
+
+# disable smart quotes
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
