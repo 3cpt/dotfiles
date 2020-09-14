956327ef3f345d4b7ee2aae9d2457a1065889ec7
André Correia <andxpto@pm.me>
2020-09-14 13:51:07 +0000

    add more defaults

--- brew.sh
+++ brew.sh
@@ -1,1 +1,15 @@
+
+# refresh brew
+brew update
+brew upgrade
+
+# apps to install
+brew install git
+brew install iterm2
+brew install node
+brew install docker
 brew install postman
+brew install robo-3t
+brew install vlc
+brew install slack
+brew install spotify
