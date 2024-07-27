4317ccd5aa700c99f4baa46bd98e64b7495ff79a
Andre Correia <andrecorreia@pm.me>
2024-07-27 21:55:08 +0100

    some more tune

--- install_linux.sh
+++ install_linux.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
+set -e # stop on error
+set +x # don't print commands
+
+DOCKER=false
+if [ "$1" = "docker" ]; then
+    DOCKER=true
+    echo "To install docker"
+fi
+
+echo "Starting install"
 sudo apt update
 sudo apt upgrade -y
 
-# Install Zsh and Oh-My-Zsh
-sudo apt install -y zsh micro curl
+# Install apt tools
+sudo apt install -y zsh micro curl htop unzip fzf
+
+# Zsh setup
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-# Set zsh as the default shell and before echo about it
-echo "zsh is now the default shell, password REQUIRED"
 sudo chsh -s $(which zsh)
-echo "zsh is now the default shell DONE"
-
-# Install oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# Install oh-my-posh and create folder if it doesn't exist
 mkdir -p $HOME/.local/bin
-sudo apt install -y unzip
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -d $HOME/.local/bin
-
-# Install fzf
-sudo apt install -y fzf
 
 # Install lazydocker
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
@@ -42,30 +42,33 @@ if [ "$(uname -m)" != "aarch64" ]; then
     brew update
 fi
 
-# Add Docker's official GPG key:
-sudo apt update
-sudo apt install -y ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+if [ "$DOCKER" = true ]; then
+    echo "Installing docker"
+    sudo apt install -y ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
-echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-sudo apt-get update
+    # Add the repository to Apt sources:
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+        sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+    sudo apt-get update
 
-# Install Docker Engine, containerd, and Docker Compose
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    # Install Docker Engine, containerd, and Docker Compose
+    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Linux post-installation steps for Docker Engine
-sudo groupadd docker
-sudo usermod -aG docker $USER
-sudo newgrp docker
+    # Linux post-installation steps for Docker Engine
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+    sudo newgrp docker
+fi
 
 # Symlink .zshrc
 sudo ln -sf $HOME/adr/dotfiles/.zshrc $HOME/.zshrc
 sudo ln -sf $HOME/adr/dotfiles/.zsh_alias $HOME/.zsh_alias
 
 exec zsh
+
+echo "Done!"
