45737b0e6a77b160fe817b2e2ab535ec1a4f6275
Andre Correia <andrecorreia@pm.me>
2024-08-16 00:19:40 +0100

    fix url

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -21,12 +21,12 @@ sudo apt-get install -y ca-certificates curl
 
 # Set up the Docker GPG key and apt repository
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL "$DOCKER_URL/gpg" -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add Docker repository to Apt sources
 echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] $DOCKER_URL \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
     sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
