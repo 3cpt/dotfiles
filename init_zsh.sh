1c3ceace1eab64054cece95d2480f3bc29b091a5
Andre Correia <andrecorreia@pm.me>
2025-04-23 13:10:03 +0100

    add k8s context to tmux

--- init_zsh.sh
+++ init_zsh.sh
@@ -84,6 +84,7 @@ ln -sf $(pwd)/configs/motivation.zsh $HOME/.oh-my-zsh/custom/motivation.zsh
 ln -sf $(pwd)/configs/.tmux.conf $HOME/.tmux.conf
 ln -sf $(pwd)/configs/config.toml $HOME/.config/atuin/config.toml
 ln -sf $(pwd)/scripts/get_custom_system_info.zsh $HOME/.local/bin/get_custom_system_info
+ln -sf $(pwd)/scripts/k8s_info_tmux.zsh $HOME/.local/bin/k8s_info_tmux
 ln -sf $(pwd)/configs/adr.zsh-theme $HOME/.oh-my-zsh/themes/adr.zsh-theme
 
 echo "Zsh setup complete. Starting new Zsh session..."
