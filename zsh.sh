95fdcb6314d901963edf26adb94d81fb760cce34
Andre Correia <andrecorreia@pm.me>
2025-06-03 14:59:53 +0100

    fix

--- zsh.sh
+++ zsh.sh
@@ -61,7 +61,7 @@ elif [ "$OS" = "Linux" ]; then
     curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 
     echo "Installing k9s"
-    wget https://github.com/derailed/k9s/releases/latest/download/k9s_linux_amd64.deb && apt install ./k9s_linux_amd64.deb && rm k9s_linux_amd64.deb
+    wget https://github.com/derailed/k9s/releases/latest/download/k9s_linux_amd64.deb && sudo apt install ./k9s_linux_amd64.deb && rm k9s_linux_amd64.deb
 else
     echo "Unsupported OS: $OS"
     exit 1
